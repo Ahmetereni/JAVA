b84e92eb962b2061d2ae3fecd3dbb10ba6889c30
Ahmetereni <94352384+Ahmetereni@users.noreply.github.com>
2022-11-26 18:44:24 +0300

    Add files via upload

---
+++ Stack2/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Stack nums = new Stack();
+        nums.push(15);
+        nums.show();
+        nums.push(7);
+        nums.show();
+        nums.push(7);
+        nums.show();
+        nums.push(7);
+        nums.show();
+        nums.pop();
+        nums.show();
+        nums.pop();
+        nums.show();
+        nums.pop();
+        nums.show();
+
+
+
+
+
+
+    }
+}
